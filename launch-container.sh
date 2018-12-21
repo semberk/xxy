21aa274c7f71bdda45e6cb93c11e3f88c68a13ea
Bin Li <bl736@cornell.edu>
2018-12-21 14:29:51 -0500

    update to  FEniCS 2018.1.0

--- launch-container.sh
+++ launch-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -v "$(pwd)":/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable:2017.2.0.r4 ". sourceme.sh; /bin/bash -i"
+docker run --rm -ti -v "$(pwd)":/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable:current ". sourceme.sh; /bin/bash -i"
